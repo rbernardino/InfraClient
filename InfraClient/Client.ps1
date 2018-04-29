0aff569a5665b016bfb4066ff3415cd3f19cb4f8
Ryan Bernardino <r.bernardino17@gmail.com>
2018-04-29 20:49:45 +0800

    Removes xDNSServerAddress 'DNSServerIP' resource as it is already set in the virtual network.

--- InfraClient/Client.ps1
+++ InfraClient/Client.ps1
@@ -1,16 +1,17 @@
 Configuration Client
 {
-  Import-DscResource -ModuleName xNetworking
+  # Import-DscResource -ModuleName xNetworking
   Import-DscResource -ModuleName xComputerManagement
 
   Node $AllNodes.Where({ $_.Purpose -eq 'Client Computer' }).NodeName
   {
-    xDNSServerAddress 'DNSServerIP'
-    {
-      Address        = '172.16.1.4'
-      AddressFamily  = 'IPv4'
-      InterfaceAlias = 'Ethernet 2'
-    }
+    # This is no longer needed as the the DNS is already set in the vnet
+    # xDNSServerAddress 'DNSServerIP'
+    # {
+    #   Address        = '172.16.1.4'
+    #   AddressFamily  = 'IPv4'
+    #   InterfaceAlias = 'Ethernet 2'
+    # }
 
     xComputer $Nodename
     {
