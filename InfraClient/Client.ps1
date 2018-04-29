0ebaacf3f6a9cd0f18f7a383d07e8ec9fc1f2709
Ryan Bernardino <r.bernardino17@gmail.com>
2018-04-29 20:53:55 +0800

    Removes xDNSServerAddress 'DNSServerIP' resource as it is already set in the virtual network.

--- InfraClient/Client.ps1
+++ InfraClient/Client.ps1
@@ -1,22 +1,14 @@
 Configuration Client
 {
-  Import-DscResource -ModuleName xNetworking
+
   Import-DscResource -ModuleName xComputerManagement
 
   Node $AllNodes.Where({ $_.Purpose -eq 'Client Computer' }).NodeName
   {
-    xDNSServerAddress 'DNSServerIP'
-    {
-      Address        = '172.16.1.4'
-      AddressFamily  = 'IPv4'
-      InterfaceAlias = 'Ethernet 2'
-    }
-
     xComputer $Nodename
     {
       Name       = $Nodename
       Credential = $(Get-Credential mycompany\winadmin)
-      DependsOn  = '[xDNSServerAddress]DNSServerIP'
       DomainName = 'mycompany.com'
     }
   }
