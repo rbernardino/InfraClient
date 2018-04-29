4dc1609fc2d20b8c14a03b7097f56d51ce52d9ed
Ryan Bernardino <r.bernardino17@gmail.com>
2018-04-29 20:52:27 +0800

    Revert "Removes xDNSServerAddress 'DNSServerIP' resource as it is already set in the virtual network."
    This reverts commit 0aff569a5665b016bfb4066ff3415cd3f19cb4f8.

--- InfraClient/Client.ps1
+++ InfraClient/Client.ps1
@@ -1,17 +1,16 @@
 Configuration Client
 {
-  # Import-DscResource -ModuleName xNetworking
+  Import-DscResource -ModuleName xNetworking
   Import-DscResource -ModuleName xComputerManagement
 
   Node $AllNodes.Where({ $_.Purpose -eq 'Client Computer' }).NodeName
   {
-    # This is no longer needed as the the DNS is already set in the vnet
-    # xDNSServerAddress 'DNSServerIP'
-    # {
-    #   Address        = '172.16.1.4'
-    #   AddressFamily  = 'IPv4'
-    #   InterfaceAlias = 'Ethernet 2'
-    # }
+    xDNSServerAddress 'DNSServerIP'
+    {
+      Address        = '172.16.1.4'
+      AddressFamily  = 'IPv4'
+      InterfaceAlias = 'Ethernet 2'
+    }
 
     xComputer $Nodename
     {
